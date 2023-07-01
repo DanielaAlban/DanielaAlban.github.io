85166682689986149c7f044ebd6354c8c188b5bd
DanielaAlban <138265212+DanielaAlban@users.noreply.github.com>
2023-06-30 20:34:34 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div class="e27_2">
+        <div  class="e27_3"></div>
+        <div  class="e27_4"></div>
+        <span  class="e27_5">Viaja a través de los Paisajes Mágicos de Ecuador</span>
+        <div  class="e27_6"></div>
+        <span  class="e27_7">Los paisajes de Ecuador son increíblemente diversos y ofrecen una amplia gama de maravillas naturales. Desde majestuosas montañas hasta exuberantes selvas tropicales y hermosas playas, este país sudamericano cautiva con su belleza y variedad.</span>
+        <div  class="e27_8"></div>
+        <span  class="e27_9">Es una laguna de origen glacial ubicada cerca del majestuoso volcán Carihuairazo, que es uno de los picos más altos de la región.
+        Esta laguna se destaca por su belleza escénica y su peculiaridad de congelarse en determinadas épocas del año debido a las bajas temperaturas.</span>
+        <div  class="e27_10"></div>
+        <span  class="e27_11">Mas información</span>
+        <div  class="e27_12"></div>
+        <span  class="e27_13">Es una laguna de origen glacial ubicada cerca del majestuoso volcán Carihuairazo, que es uno de los picos más altos de la región.
+        Esta laguna se destaca por su belleza escénica y su peculiaridad de congelarse en determinadas épocas del año debido a las bajas temperaturas.</span>
+        <div  class="e27_14"></div>
+        <div class="e27_15">
+          <div  class="e27_16"></div>
+        </div>
+        <span  class="e27_17">Mas información</span>
+        <div  class="e27_18"></div>
+        <span  class="e27_19">Mas información</span>
+        <div  class="e27_21"></div>
+        <div  class="e27_22"></div>
+        <span  class="e27_23">El desierto del río Jubones es un lugar de extremos y contradicciones. Agreste y seco. Ubicado entre las provincias de Azuay, Loja y El Oro.</span>
+        <div class="e27_24">
+          <div  class="e27_25"></div>
+        </div>
+        <span  class="e27_26">Mas información</span>
+        <div class="e27_27">
+          <div  class="e27_28"></div>
+          <div class="e27_29">
+            <div  class="ei27_29_27_25"></div>
+          </div>
+          <span  class="e27_30">Mas información</span>
+        </div>
+        <div  class="e27_31"></div>
+        <span  class="e27_32">El Pailon del Diablo se localiza en la provincia de Tungurahua específicamente en la hermosa Ciudad de Baños en la Parroquia Río Verde de ahí su nombre original de Cascada Río Verde.</span>
+        <div  class="e27_33"></div>
+        <div  class="e27_34"></div>
+        <div class="e27_35">
+          <div  class="ei27_35_27_25"></div>
+        </div>
+        <span  class="e27_36">La Playa de Santa Marianita tiene una extensión aproximada de 4 km, está ubicado a veinte minutos al sur de la ciudad de Manta, entre el Cabo de San Lorenzo y la Punta de San Mateo.</span>
+        <span  class="e27_37">Mas información</span>
+        <div  class="e27_38"></div>
+        <div  class="e27_39"></div>
+        <span  class="e27_40">Las Cavernas de Jumandy es un hermoso y misterioso lugar turístico Localizada a 2 kilómetros de distancia (10 minutos) de la ciudad de Archidona, vía Baeza- Quito.</span>
+        <div class="e27_41">
+          <div  class="ei27_41_27_25"></div>
+        </div>
+        <span  class="e27_42">Mas información</span>
+        <div  class="e27_43"></div>
+        <div  class="e27_44"></div>
+        <span  class="e27_45">En La provincia de Pichincha y a 13,5 Km. de Quito se encuentra la ciudad de la mitad del mundo.Se trata de una pequeña ciudad cuya actividad gira en torno al paralelo 0, punto geodésico 0-0’-0’’ .</span>
+        <div class="e27_46">
+          <div  class="ei27_46_27_25"></div>
+        </div>
+        <span  class="e27_47">Mas información</span>
+        <div  class="e27_48"></div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
